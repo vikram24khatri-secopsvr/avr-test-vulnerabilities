c86b7610b74fa4f951ab2988838aae8515587563
vikram24khatri-secopsvr <vikram24.khatri@gmail.com>
2026-06-26 15:14:41 +1000

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+javascriptconst express = require('express');
+const app = express();
+
+app.get('/run', (req, res) => {
+    // CRITICAL SAST INJECTION: Direct execution of raw user input
+    eval(req.query.userInput); 
+    res.send('Executed!');
+});
