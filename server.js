5d53050af54bc3a47d137bd03a7d6307b79ca72c
vikram24khatri-secopsvr <vikram24.khatri@gmail.com>
2026-06-26 15:46:34 +1000

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/run', (req, res) => {
-    // CRITICAL SAST INJECTION: Direct execution of raw user input
-    eval(req.query.userInput); 
-    res.send('Executed!');
-});
